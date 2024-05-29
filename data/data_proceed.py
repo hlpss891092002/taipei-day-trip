429ef875685b0ce92527e938b4ec940e9c1f1921
hlpss891092002 <146625541+hlpss891092002@users.noreply.github.com>
2024-05-30 07:00:26 +0800

    modify data_proceed

--- data/data_proceed.py
+++ data/data_proceed.py
@@ -1,17 +1,57 @@
 import mysql.connector
 import json
 
+mydb = mysql.connector.connect(
+  host = "localhost",
+  user = "root",
+  password = "00000000",
+)
+mycursor1 = mydb.cursor()
+#create db 
+mycursor1.execute("""CREATE DATABASE wehelp_stage2_taipei_spot""")
+
 # load in db
 mydb = mysql.connector.connect(
   host = "localhost",
   user = "root",
-  password = "0000",
+  password = "00000000",
   database = "wehelp_stage2_taipei_spot",
 )
   
 mycursor = mydb.cursor()
 
-mycursor.execute("""""")
+
+#create TABLE taipei_attraction
+mycursor.execute("""CREATE TABLE taipei_attraction(
+                    id BIGINT AUTO_INCREMENT,
+                    name varchar(255),
+                    MRT varchar(255),
+                    address varchar(255),
+                    rate int,
+                    direction varchar(2000),
+                    description varchar(2000),
+                    CAT varchar(255),
+                    MEMO_TIME varchar(500),
+                    longitude float,
+                    latitude float,
+                    primary key(id)
+)""")
+#create TABLE photo_file
+mycursor.execute("""CREATE TABLE photo_file(
+                 id BIGINT AUTO_INCREMENT,
+                 attraction_id BIGINT,
+                 photo varchar(500),
+                 PRIMARY KEY(id),
+                 FOREIGN KEY photo_file(attraction_id) 	REFERENCES taipei_attraction(id)
+)""")
+# CREATE TABLE MRT_LIS
+mycursor.execute("""CREATE TABLE mrt_list(
+	id BIGINT AUTO_INCREMENT,
+	MRT_name varchar(255),
+	spot_count int,
+primary KEY (id)
+)""")
+
 
 def insert_value_in_taipei_table(spot):
   name = spot["name"]
@@ -61,6 +101,47 @@ for spot in all_data:
   print(photo_list)
   count+=1
  
+def get_distinctMRT ():
+  mycursor = mydb.cursor()
+  MRT_List = []
+  mycursor.execute("SELECT DISTINCT MRT fROM taipei_attraction ")
+  result = mycursor.fetchall()
+  for MRT in result:
+    if MRT[0] == "None":
+      continue
+    else:
+      MRT_List.append(MRT[0])
+  return MRT_List
+
+def get_MRT_spot_NUM(MRT_List):
+  mycursor = mydb.cursor()
+  M_C_dict = {}
+  for MRT in MRT_List:
+    sql = "SELECT COUNT(*) fROM taipei_attraction WHERE MRT = %s"
+    val = (f"{MRT}",)
+    mycursor.execute(sql, val)
+    count_num = mycursor.fetchone()[0]
+    M_C_dict[f"{MRT}"] = count_num
+    # print(M_C_dict)
+  return M_C_dict
+    
+def insert_MRT_count(MRT_List, M_C_dict):
+  mycursor = mydb.cursor()
+  for MRT in MRT_List:
+    count = M_C_dict[f"{MRT}"]
+    sql = "INSERT INTO mrt_list(MRT_name, spot_count) values (%s, %s)"
+    val = (f"{MRT}",count)
+    print(f"{MRT}")
+    print(count)
+    mycursor.execute(sql, val)
+    print(mycursor.rowcount, "record inserted in mrt_List.")
+    mydb.commit()
+
+# create mrt_list_table    
+MRT_List= get_distinctMRT()
+print(MRT_List)
+M_C_dict = get_MRT_spot_NUM(MRT_List)
+insert_MRT_count(MRT_List, M_C_dict)
 
 
 
--- db_search.py
+++ db_search.py
@@ -5,52 +5,12 @@ import json
 mydb = mysql.connector.connect(
   host = "localhost",
   user = "root",
-  password = "0000",
+  password = "00000000",
   database = "wehelp_stage2_taipei_spot",
 )
 
 
-def get_distinctMRT ():
-  mycursor = mydb.cursor()
-  MRT_List = []
-  mycursor.execute("SELECT DISTINCT MRT fROM taipei_attraction ")
-  result = mycursor.fetchall()
-  for MRT in result:
-    if MRT[0] == "None":
-      continue
-    else:
-      MRT_List.append(MRT[0])
-  return MRT_List
 
-def get_MRT_spot_NUM(MRT_List):
-  mycursor = mydb.cursor()
-  M_C_dict = {}
-  for MRT in MRT_List:
-    sql = "SELECT COUNT(*) fROM taipei_attraction WHERE MRT = %s"
-    val = (f"{MRT}",)
-    mycursor.execute(sql, val)
-    count_num = mycursor.fetchone()[0]
-    M_C_dict[f"{MRT}"] = count_num
-    # print(M_C_dict)
-  return M_C_dict
-    
-def insert_MRT_count(MRT_List, M_C_dict):
-  mycursor = mydb.cursor()
-  for MRT in MRT_List:
-    count = M_C_dict[f"{MRT}"]
-    sql = "INSERT INTO mrt_list(MRT_name, spot_count) values (%s, %s)"
-    val = (f"{MRT}",count)
-    print(f"{MRT}")
-    print(count)
-    mycursor.execute(sql, val)
-    print(mycursor.rowcount, "record inserted in mrt_List.")
-    cnx1.commit()
-
-#create mrt_list_table    
-# MRT_List= get_distinctMRT()
-# print(MRT_List)
-# M_C_dict = get_MRT_spot_NUM(MRT_List)
-# insert_MRT_count(MRT_List, M_C_dict)
 
 
 def get_MRT_ORDERBY_spot_count():
