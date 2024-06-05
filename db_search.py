47d14ff866db3b6d3181b47cad18cb4d2b04f1ef
hlpss891092002 <146625541+hlpss891092002@users.noreply.github.com>
2024-06-05 19:53:38 +0800

    algin element edge

--- db_search.py
+++ db_search.py
@@ -3,8 +3,8 @@ import mysql.connector.pooling
 import json
 
 dbconfig = {
-    "host":"localhost",
-    "user":"root",
+    "host":"0.0.0.0",
+    "user":"lucas",
     "password":"00000000",
     "database":"wehelp_stage2_taipei_spot",
 }
