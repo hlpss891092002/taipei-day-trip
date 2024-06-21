dde177311f6f07d74cba6484725b603c09d523e2
hlpss891092002 <146625541+hlpss891092002@users.noreply.github.com>
2024-06-21 15:42:10 +0800

    add app loging

--- test.py
+++
@@ -1,5 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-Format = ' %(asctime)s - %(message)s'
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO, format=Format)
-logger.info('This message should go to the log file')
